ffe4797799f459461de6d698b090f4a3af3503ad
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-10-05 13:21:16 +0200

    Create getUser method, action, mutation.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import githubApi from '../apis/githubApi';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        user: {}
+    },
+    mutations: {
+        fillUser(state, data) {
+            state.user = data
+            console.log(data)
+        }
+    },
+    actions: {
+        getUser({ commit }, payload) {
+
+            githubApi.get(`/users/${payload}`)
+                .then(res => {
+                    commit("fillUser", res.data)
+                })
+                .catch(err => console.log(err))
+        }
+    },
+    modules: {}
 });
--- src/views/SearchUser.vue
+++ src/views/SearchUser.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="container">
-    <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="username"
-            :rules="userRules"
-            :counter="10"
-            label="Type user name"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-btn
-      rounded
-      color="orange lighten-1"
-      dark
-    >
-      Search
-    </v-btn>
-    </v-container>
-  </v-form>
+    <v-form v-model="valid" @submit.prevent="searchUser">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="username"
+              :rules="userRules"
+              :counter="10"
+              label="Type user name"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn rounded color="orange lighten-1" dark type="submit">
+          Search
+        </v-btn>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
@@ -32,12 +25,17 @@
 export default {
   name: "SearchUser",
   data: () => ({
-      valid: false,
-      username: '',
-      userRules: [
-        v => !!v || 'Username is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters',
-      ]
-    })
+    valid: false,
+    username: "",
+    userRules: [
+      v => !!v || "Username is required",
+      v => v.length <= 99 || "Name must be less than 99 characters"
+    ]
+  }),
+  methods: {
+    searchUser() {
+      this.$store.dispatch("getUser", this.username);
+    }
+  }
 };
 </script>
