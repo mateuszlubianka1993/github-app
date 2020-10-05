eb3aa895dd02932645d54f496729f9ae04992375
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-10-05 17:06:29 +0200

    Add Footer component.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,51 +1,33 @@
 <template>
-    <v-footer
-      v-bind="localAttrs"
-      class="pa-0"
-      fixed
-    >
-      <v-card
-        flat
-        tile
-        width="100%"
-        class="orange lighten-1 text-center"
-      >
-        <v-card-text class="white--text">
-          {{ new Date().getFullYear() }} — <strong>Github App</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+  <v-footer v-bind="localAttrs" class="pa-0" fixed>
+    <v-card flat tile width="100%" class="orange lighten-1 text-center">
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Github App</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      icons: [
-        'mdi-home',
-        'mdi-email',
-        'mdi-calendar',
-        'mdi-delete',
-      ],
-      items: [
-        'default',
-        'absolute',
-        'fixed',
-      ],
-      padless: false,
-      variant: 'default',
-    }),
-    computed: {
-      localAttrs () {
-        const attrs = {}
+export default {
+  data: () => ({
+    icons: ["mdi-home", "mdi-email", "mdi-calendar", "mdi-delete"],
+    items: ["default", "absolute", "fixed"],
+    padless: false,
+    variant: "default"
+  }),
+  computed: {
+    localAttrs() {
+      const attrs = {};
 
-        if (this.variant === 'default') {
-          attrs.absolute = false
-          attrs.fixed = false
-        } else {
-          attrs[this.variant] = true
-        }
-        return attrs
-      },
-    },
+      if (this.variant === "default") {
+        attrs.absolute = false;
+        attrs.fixed = false;
+      } else {
+        attrs[this.variant] = true;
+      }
+      return attrs;
+    }
   }
+};
 </script>
