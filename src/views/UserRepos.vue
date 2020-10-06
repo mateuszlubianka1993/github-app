1ccfc263632dba2fe4d5a444eeb313fa181cb016
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-10-06 12:35:28 +0200

    Create UserRepos vue.

--- src/views/UserRepos.vue
+++ src/views/UserRepos.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="container">
-      <h2 class="text-center"><v-icon style="font-size: 100px">mdi-altimeter</v-icon> {{ user }} repositories list</h2>
-    </div>
+  <div class="container">
+    <h2 class="text-center">
+      <v-icon style="font-size: 100px">mdi-altimeter</v-icon>
+      {{ user }} repositories list
+    </h2>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "UserRepos",
-    props: ['user']
-}
+  name: "UserRepos",
+  props: ["user"]
+};
 </script>
