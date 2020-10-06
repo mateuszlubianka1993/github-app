98096f018c2ce43bc13f9d5e2029158e2c302159
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-10-06 11:13:01 +0200

    Remove computed function from Footer component.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,23 +11,8 @@
 <script>
 export default {
   data: () => ({
-    icons: ["mdi-home", "mdi-email", "mdi-calendar", "mdi-delete"],
-    items: ["default", "absolute", "fixed"],
     padless: false,
     variant: "default"
-  }),
-  computed: {
-    localAttrs() {
-      const attrs = {};
-
-      if (this.variant === "default") {
-        attrs.absolute = false;
-        attrs.fixed = false;
-      } else {
-        attrs[this.variant] = true;
-      }
-      return attrs;
-    }
-  }
+  })
 };
 </script>
