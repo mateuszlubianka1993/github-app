0125319a2cde4bb62262fe8aba3196b9ba265b7e
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-10-07 12:40:52 +0200

    Create UserRepos store, actions, mutations, getters and use data in the component.

--- src/views/UserRepos.vue
+++ src/views/UserRepos.vue
@@ -4,12 +4,26 @@
       <v-icon style="font-size: 100px">mdi-altimeter</v-icon>
       {{ user }} repositories list
     </h2>
+    <div class="list" v-if="repos.length > 0">
+        <div class="item" v-for="repo in repos" :key="repo.full_name">
+            {{repo.full_name}}
+        </div>
+    </div>
+    <div v-else>There is nothing here!</div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "UserRepos",
-  props: ["user"]
+  props: ["user"],
+  created() {
+      this.$store.dispatch("getUserRepos", this.user);
+  },
+  computed: {
+    ...mapGetters(["repos"])
+  }
 };
 </script>
